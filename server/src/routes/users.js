93c05ec6a834982363a45b9de2306e0637a413a6
AUnderwood9 <aunderwood797@gmail.com>
2018-04-02 00:50:50 -0500

    Created some of the controllers we will need. Also created some of the base routes for the Users route and table.

--- server/src/routes/users.js
+++ server/src/routes/users.js
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import { tokenMiddleware, isLoggedIn } from '../middleware/auth.mw';
+import * as userController from "../controllers/userController";
 
 let router = Router();
 
@@ -7,5 +8,52 @@ router.get('/me', tokenMiddleware, isLoggedIn, (req, res) => {
     console.log("routing")
     res.json(req.user);
 });
+
+router.get("/", (req, res, body) => {
+    userController.getUsers()
+    .then((Response) => {
+        res.send(Response);
+    })
+    .catch((err) => {
+        console.log(err);
+        res.sendStatus(400);
+    })
+})
+
+router.get("/:id", (req, res, body) => {
+    userController.getUser(req.params.id)
+    .then((Response) => {
+        res.send(Response);
+    })
+    .catch((err) => {
+        console.log(err);
+        res.send(404);
+    })
+})
+
+router.post("/newuser", (req, res, body) => {
+    // console.log(req.body);
+    // res.send("stuff");
+    // userController.addUser(req.body)
+    userController.addUser([req.body.fname, req.body.lname, req.body.email, req.body.password, req.body.handle, req.body.avatar])
+    .then((response) => {
+        res.send(response);
+    })
+    .catch((err) => {
+        console.log(err);
+        res.sendStatus(403);
+    })
+})
+
+router.delete("/:id", (req, res, body) => {
+    userController.deleteUser(req.params.id)
+    .then((response) => {
+        res.send(response);
+    })
+    .catch((err) => {
+        console.log(err);
+        res.sendStatus(403);
+    })
+})
 
 export default router;
