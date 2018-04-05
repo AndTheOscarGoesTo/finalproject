fbb978eaf8d49c24c08a3741ddcf0de2c7b48c52
clewis2211 <aunderwood797@gmail.com>
2018-04-05 17:41:36 -0500

    Fixing merge conflicts.

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -5,13 +5,8 @@ import statusRouter from './status';
 import igdbRouter from "./games"; 
 import ImageUploadRouter from './ImageUploaded';
 import relationshipsRouter from './relationships';
-<<<<<<< HEAD
-import profileRouter from './profiles';
-import forumsRouter from './forum';
-=======
 import profileRouter from "./profiles";
 import forumsRouter from "./forum";
->>>>>>> 67f167ad5ffd1dafba27b792509f5f841ffbf2c1
 import { isLoggedIn, tokenMiddleware } from '../middleware/auth.mw';
 
 
@@ -20,10 +15,7 @@ router.use('/auth', authRouter);
 router.use('/users', usersRouter);
 router.use('/status', statusRouter);
 router.use("/games", igdbRouter);
-<<<<<<< HEAD
-=======
 router.use("/ImageUploaded", ImageUploadRouter);
->>>>>>> 67f167ad5ffd1dafba27b792509f5f841ffbf2c1
 router.use('/relationships', relationshipsRouter);
 router.use("/profile", profileRouter);
 router.use("/forums", forumsRouter);
