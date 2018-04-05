748425e548dbe906aa84037f42223a06434c6008
clewis2211 <aunderwood797@gmail.com>
2018-04-05 09:51:58 -0500

    Fixed some merge conflicts. Also added routes to get games by their id, name, and genre.

--- server/src/routes/games.js
+++ server/src/routes/games.js
@@ -8,6 +8,14 @@ const router = Router();
 
 router.get("/", (req, res, body) => {
     // console.log("--by platform id--", req.query.byPlatformId);
+
+    /* 
+        Example requests: 
+            byPlatformId: localhost:3000/api/games?byPlatformId=1
+            byGenre: localhost:3000/api/games?byGenre=action
+            byGameName: localhost:3000/api/games?byGameName=call of duty
+            byGameId: localhost:3000/api/games?byGameId=1
+    */
     if(req.query.byPlatformId && !isNaN(req.query.byPlatformId)){
         // console.log("--by platform id in query--", req.query.byPlatformId);
         // res.send("searching by platform");
@@ -16,6 +24,42 @@ router.get("/", (req, res, body) => {
             // let parsed = JSON.parse(parser.toJson(response)).Data.Game;
             let parsed = JSON.parse(parser.toJson(response)).Data.Game;
             // console.log(parsed);
+            res.send(parsed);
+        })
+        .catch((err) => {
+            console.log(err);
+            res.sendStatus(403);
+        })
+    } else if(req.query.byGenre && isNaN(req.query.byGenre)){
+        // res.send(`Genre: ${req.query.byGenre}`);
+        gameDbController.getGamesByGenre(req.query.byGenre)
+        .then((response) => {
+            let parsed = JSON.parse(parser.toJson(response)).Data.Game;
+            console.log(parsed);
+            res.send(parsed);
+        })
+        .catch((err) => {
+            console.log(err);
+            res.sendStatus(403);
+        })
+    } else if(req.query.byGameName && isNaN(req.query.byGameName)){
+        // res.send(`Game Name: ${req.query.byGameName}`);
+        gameDbController.getGameByString(req.query.byGameName)
+        .then((response) => {
+            let parsed = JSON.parse(parser.toJson(response)).Data.Game;
+            console.log(parsed);
+            res.send(parsed);
+        })
+        .catch((err) => {
+            console.log(err);
+            res.sendStatus(403);
+        })
+    } else if(req.query.byGameId && !isNaN(req.query.byGameId)){
+        // res.send(`Game Id: ${req.query.byGameId}`);
+        gameDbController.getGameById(req.query.byGameId)
+        .then((response) => {
+            let parsed = JSON.parse(parser.toJson(response)).Data.Game;
+            console.log(parsed);
             res.send(parsed);
         })
         .catch((err) => {
@@ -36,18 +80,6 @@ router.get("/", (req, res, body) => {
         })
     }
 })
-
-// router.get("/", (req, res, body) => {
-//     gamesDbController.getGames()
-//     .then(response => response.json())
-//     .then((response) => {
-//         res.send(response);
-//     })
-//     .catch((err) => {
-//         console.log(err);
-//         res.sendStatus(403);
-//     })
-// });
 
 router.get("/:gameString", (req, res, body) => {
     console.log("--params--", req.params.gameString)
