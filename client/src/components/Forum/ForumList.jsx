489fb1aae7c5452a1f9f256a6ca5f8569f01dc97
Mattengin <matt6mub@gmail.com>
2018-04-10 18:00:43 -0500

    fixed forum

--- client/src/components/Forum/ForumList.jsx
+++ client/src/components/Forum/ForumList.jsx
@@ -2,17 +2,25 @@ import React, { Component, Fragment } from 'react';
 import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 import { get, post } from '../../services/base'
 import style from './Forum.module.scss'
-
+import {Accordion, PanelGroup, Panel} from 'react-bootstrap';
 
 class ForumList extends Component {
 
-    constructor(props){
-      super(props);
-      this.state = {
-        forumz: [],
-      }
-    //    this.handleselect.bind(this)
-    }
+  constructor(props, context) {
+    super(props, context);
+
+    this.handleSelect = this.handleSelect.bind(this);
+
+    this.state = {
+      activeKey: '',
+      forumz: [],
+    };
+  }
+
+  handleSelect(activeKey) {
+    this.setState({ activeKey });
+  }
+  
 
     componentDidMount(){
       get('http://localhost:3000/api/forums')
@@ -20,14 +28,13 @@ class ForumList extends Component {
       .then(log => console.log(this.state.forumz))
     }
 
-
     
 
     render() {
       let forumz = this.state.forumz.map((forumz) => {
         return(
-<div id="accordion">
-<div class="card">
+/* <div id="accordion">
+<div class="card" onClick= { (event) => {this.handleselect(event)}}>
     <div class="card-header" >
       <h5 class="mb-0">
         <h1 class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapse" aria-expanded="false" aria-controls="collapse">
@@ -43,7 +50,24 @@ class ForumList extends Component {
   
   </div>
   
-</div>
+</div> */
+
+<PanelGroup
+        accordion
+        id="accordion-controlled-example"
+        activeKey={this.state.activeKey}
+        onSelect={this.handleSelect}
+      >
+        <Panel eventKey={forumz.id}
+        key={forumz.id}
+        >
+          <Panel.Heading>
+            <Panel.Title toggle>{forumz.title}</Panel.Title>
+          </Panel.Heading>
+          <Panel.Body collapsible>{forumz.forumText}</Panel.Body>
+        </Panel>
+      </PanelGroup>
+
         )
       })
         return (
