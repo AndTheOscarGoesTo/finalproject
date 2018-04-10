2e61999c616dcf848562c30a3cb2fd86c54d0ccc
clewis2211 <aunderwood797@gmail.com>
2018-04-09 22:07:58 -0500

    Added styling to the search component. Will also add more information to the results as needed.

--- client/src/components/ProfileSearchContainer/ProfileSearchContainer.jsx
+++ client/src/components/ProfileSearchContainer/ProfileSearchContainer.jsx
@@ -1,15 +1,45 @@
 import React, { Component } from "react";
+import { get } from "../../services/base";
+
+import ProfileSearchListing from "./ProfileSearchListing";
 
 class ProfileSearchContainer extends Component{
     constructor(props){
         super(props);
-
+        
+        this.state = {
+            profiles: []
+        }
 
     }
 
+    componentDidMount(){
+
+        let profiles = []
+
+        get(`http://localhost:3000/api/users/handle/${this.props.match.params.searchString}`)
+        .then((response) => {
+            // console.log(response);
+            response.forEach((item) => {
+                profiles.push({
+                    avatar: item.avatar,
+                    userId: item.id,
+                    userHandle: item.handle
+                })
+            })
+
+            this.setState({ profiles })
+        })
+        .catch((err) => {
+            console.log(err);
+        })
+    }
+
     render(){
+        // console.log("--profiles--", this.state.profiles);
         return(
-            <h1>String you gave {this.props.match.params.searchString}</h1>
+            // <h1>String you gave {this.props.match.params.searchString}</h1>
+            <ProfileSearchListing listingInfo={this.state.profiles} />
         )
     }
 }
---
+++ client/src/components/ProfileSearchContainer/ProfileSearchListing.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import Style from "./ProfileSearchContainer.module.scss";
+
+function ProfileSearchListing(props){
+
+    console.log("--props--", props.listingInfo)
+
+    return(
+        <div className={`jumbotron`}>
+
+            <div className="container">
+            {
+                props.listingInfo.map((item, index) => {
+                    return(
+                        <div className={`panel ${Style.panelParent}`} key={`user-search-index-${index}-user-${item.userId}`}>
+
+                            <Link to={`/profile/${item.userId}`}>
+                                <img className={`img-thumbnail ${Style.imgThumb}`} src={item.avatar} alt=""/>
+                            </Link>
+                            <h1>@{item.userHandle}</h1>
+            
+                        </div>
+                    );
+                })
+            }
+                {/* <div className="panel">
+                    <Link to={`/profile/${props.listingInfo.userId}`}>
+                        <img src={props.listingInfo.avatar} alt=""/>
+                    </Link>
+                    
+                    <h1>{props.listingInfo.userHandle}</h1>
+                </div> */}
+            
+            </div>
+
+        </div>
+    );
+
+}
+
+export default ProfileSearchListing;
