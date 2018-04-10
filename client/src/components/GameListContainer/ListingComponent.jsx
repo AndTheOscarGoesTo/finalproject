80d3c9373857441c2d5f058fe4397bd4507fbfce
clewis2211 <aunderwood797@gmail.com>
2018-04-10 12:02:06 -0500

    Added pagination to game lists. There is a bug where you cannot go back on page 2 and you have to go forward twice on the first page.

--- client/src/components/GameListContainer/ListingComponent.jsx
+++ client/src/components/GameListContainer/ListingComponent.jsx
@@ -1,7 +1,9 @@
 import React, { Fragment, Component } from "react";
 import GameListing from "../ListingPage/ListingPage";
 import { get } from "../../services/base";
+
 import LoadingComponent from "../LoadingViewContainer/LoadingViewContainer";
+import Paginator from "../Paginator/Paginator";
 
 class ListingComponent extends Component{
     constructor(props){
@@ -10,11 +12,25 @@ class ListingComponent extends Component{
         this.state = {
             games: [],
             loading: false,
-            working: false
+            working: false,
+            limit: 10,
+            offset: 0,
+            listLimit: 0
         }
+
+        this.onGetNextGames = this.onGetNextGames.bind(this);
+        this.onGetPreviosGames = this.onGetPreviosGames.bind(this);
+
     }
 
     componentDidMount(){
+
+        this.requestGames();
+    }
+
+    requestGames(){
+
+        console.log("--requesting--");
 
         const alias = (this.props.location.state.alias) ? this.props.location.state.alias : null;
         const searchString = (this.props.location.state.searchString) ? this.props.location.state.searchString : null;
@@ -26,27 +42,24 @@ class ListingComponent extends Component{
         })
 
         if(alias){
-            console.log("--alias--", alias);
-            get(`http://localhost:3000/api/games?byPlatformName=${alias}`)
+            // console.log("--alias--", alias);
+            // get(`http://localhost:3000/api/games?byPlatformName=${alias}`)
+            get(`http://localhost:3000/api/games?byPlatformName=${alias}&limit=${this.state.limit}&offset=${this.state.offset}`)
             .then((response) => {
 
                 let games = [];
 
-                response.map((item, index) => {
-
-                    if(item.hasOwnProperty("thumb") && item.hasOwnProperty("ReleaseDate")){
-
-                        games.push({ 
-                            gameId: item.id,
-                            title: item.GameTitle, 
-                            releaseDate: item.ReleaseDate, 
-                            thumbnail: item.thumb});
-                    }
-                    
+                response.gamesList.map((item, index) => {
+                    games.push({ 
+                        gameId: item.id,
+                        title: item.GameTitle, 
+                        releaseDate: item.ReleaseDate, 
+                        thumbnail: item.thumb});
                 })
 
                 this.setState({ 
                     games,
+                    listLimit: response.listSize,
                     loading: !this.state.loading,
                     working: !this.state.working
                  });
@@ -55,27 +68,24 @@ class ListingComponent extends Component{
                 console.error(err);
             })
         } else {
-            get(`http://localhost:3000/api/games?byGameName=${searchString}`)
+            get(`http://localhost:3000/api/games?byGameName=${searchString}&limit=${this.state.limit}&offset=${this.state.offset}`)
             .then((response) => {
 
                 let games = [];
 
-                response.map((item) => {
-
-                    if(item.Images && item.Images.boxart.hasOwnProperty("thumb")){
-                        games.push({ 
-                            gameId: item.id, 
-                            title: item.GameTitle, 
-                            releaseDate: item.ReleaseDate, 
-                            thumbnail: item.Images.boxart.thumb});
-                    }
-                    
+                response.gamesList.map((item) => {
+                    games.push({ 
+                        gameId: item.id, 
+                        title: item.GameTitle, 
+                        releaseDate: item.ReleaseDate, 
+                        thumbnail: item.Images.boxart.thumb});
                 })
 
                 this.setState({ 
-                        games,
-                        loading: !this.state.loading,
-                        working: !this.state.working
+                    games,
+                    listLimit: response.listSize,
+                    loading: !this.state.loading,
+                    working: !this.state.working
                     });
             })
             .catch((err) => {
@@ -90,7 +100,33 @@ class ListingComponent extends Component{
         }
     }
 
+    onGetPreviosGames(event){
+        
+        if(this.state.offset - this.state.limit >= 0){
+            console.log("--previous--");
+            this.setState({
+                offset: this.state.offset - 10
+            })
+
+            this.requestGames();
+        }
+    }
+
+    onGetNextGames(event){
+        
+        if(this.state.offset + this.state.limit <= this.state.listLimit){
+            console.log("--next--");
+            this.setState({
+                offset: this.state.offset + 10
+            })
+
+            this.requestGames();
+        }
+    }
+
     render(){
+
+        console.log("--rendering--");
 
         return(
             <Fragment>
@@ -104,6 +140,8 @@ class ListingComponent extends Component{
                 <LoadingComponent loading={this.state.loading} />
 
                 <GameListing currentList={this.state.games}/>
+
+                <Paginator getNextGames={this.onGetNextGames} getPreviosGames={this.onGetPreviosGames} />
 
             </Fragment>
         );
