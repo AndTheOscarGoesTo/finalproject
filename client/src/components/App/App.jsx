aceee5d06243d7f8d187c9ef9e59e6756ee88cff
clewis2211 <aunderwood797@gmail.com>
2018-04-09 21:23:23 -0500

    Fixed the routes. They will now keep user login information and will no longer ask for anyone to login on load.

--- client/src/components/App/App.jsx
+++ client/src/components/App/App.jsx
@@ -32,17 +32,17 @@ class Navigation extends Component {
                         <Route path="/newuser" component={NewUser} />
                         <Route exact path="/profile/:id" render={routeProps => <Profile {...routeProps} yeet={'yeet'} />} />
                         {/* <Route exact path="/profile/:id" component={Profile} /> */}
-                        <Route path="/forum" component={Forum} />
-                        <Route path="/GameCatalogue" component={GameListContainer}/>
-                        <Route path="/forumpage" component={Forumpage} />
+                        <PrivateRoute path="/forum" component={Forum} />
+                        <PrivateRoute path="/GameCatalogue" component={GameListContainer}/>
+                        <PrivateRoute path="/forumpage" component={Forumpage} />
                         
-                        <Route path="/forumpage/:id" component={Forumpage} />
-                        <Route path="/postforum" component={PostForum} />
-                        <Route path="/social/:id" component={ Social } />
-                        <Route path="/edit/:id" component={ EditSocial } />
-                        <Route path="/add/social/:id" component={ AddSocial } />
+                        <PrivateRoute path="/forumpage/:id" component={Forumpage} />
+                        <PrivateRoute path="/postforum" component={PostForum} />
+                        <PrivateRoute path="/social/:id" component={ Social } />
+                        <PrivateRoute path="/edit/:id" component={ EditSocial } />
+                        <PrivateRoute path="/add/social/:id" component={ AddSocial } />
 
-                        <Route path="/MyGameList" component={UserGameList} />
+                        <PrivateRoute path="/MyGameList" component={UserGameList} />
                         <PrivateRoute path="/goodbye" component={GoodbyeWorld} />
                     </Switch>
                 </Fragment>
