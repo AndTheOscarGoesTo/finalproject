319fb86482c423ce5cb64a547f4d52927ddb418a
clewis2211 <aunderwood797@gmail.com>
2018-04-10 20:40:32 -0500

    Added some styling.

--- client/src/components/Paginator/Paginator.jsx
+++ client/src/components/Paginator/Paginator.jsx
@@ -4,14 +4,16 @@ import Style from "./Paginator.module.scss";
 function Paginator(props) {
     return (
         <div className={`${Style.paginatorContianer}`}>
+        {/*glyphicon glyphicon-backward ion-ios-arrow-left */}
             <span 
-                className={`glyphicon glyphicon-backward ${Style.arrow}`}
+                className={`ion-ios-arrow-left ${Style.arrow}`}
                 onClick={ (event) => props.getPreviosGames(event) }
             >
                 </span> 
                     <span className="glyphicon glyphicon-option-vertical"></span> 
+                    {/* ion-ios-arrow-right glyphicon glyphicon-forward*/}
             <span 
-                className={`glyphicon glyphicon-forward ${Style.arrow}`}
+                className={`ion-ios-arrow-right ${Style.arrow}`}
                 onClick={ (event) => props.getNextGames(event) }
             ></span>
         </div>
